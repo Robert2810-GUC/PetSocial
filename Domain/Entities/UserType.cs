df60ca93b73c598fe430034d44225495db32fb88
Robert2810-GUC <mgdfr2810@gmail.com>
2025-08-08 16:55:41 +0530

    Add image path and description to user types

--- Domain/Entities/UserType.cs
+++ Domain/Entities/UserType.cs
@@ -1,11 +1,11 @@
-﻿using System.Numerics;
-
-namespace Domain.Entities;
+﻿namespace Domain.Entities;
 
 public class UserType
 {
     public long Id { get; set; }
     public string Name { get; set; }
+    public string ImagePath { get; set; }
+    public string Description { get; set; }
 
     public ICollection<User> Users { get; set; }
 }
