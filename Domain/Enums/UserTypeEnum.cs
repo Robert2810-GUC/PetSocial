cec7361fb69f4c0862c7b77174fa0cd3b71b515a
ShubhanshJaiswal <codingbrains36@gmail.com>
2025-08-12 18:36:55 +0530

    pettypeid bug fix

--- Domain/Enums/UserTypeEnum.cs
+++ Domain/Enums/UserTypeEnum.cs
@@ -1,14 +1,16 @@
 ﻿namespace Domain.Enums;
 
 public enum UserTypeEnum
-{   
-    // Main Types
-    Admin = 1,
-    User = 2,
+{
+        // User Subtypes (start from 100 range to avoid overlap)
+    PetOwner = 1,
+    PetBusiness = 2,
+    ContentCreator = 3,
 
-    // User Subtypes (start from 100 range to avoid overlap)
-    PetOwner = 101,
-    PetBusiness = 102,
-    ContentCreator = 103
+
+    // Main Types
+    Admin = 101,
+    User = 102
+    
 }
 
